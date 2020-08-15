2bb970ca28e7e5c05e1ac8cf0b248e31931a52cf
Graham Miller <mail@grahammiller.net>
2020-08-15 10:49:14 +0100

    Complete

---
+++ BowlingKata.Test/GameTests.cs
@@ -0,0 +1,91 @@
+﻿using NUnit.Framework;
+
+namespace BowlingKata.Test
+{
+    [TestFixture]
+    public class GameTests
+    {
+        private Game _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _sut = new Game();
+        }
+
+        [Test]
+        public void TestGame()
+        { }
+
+        [Test]
+        public void TestScore()
+        {
+            RollMany(20, 0);
+            AssertScoreIs(0);
+        }
+
+        [Test]
+        public void TestAllOne()
+        {
+            RollMany(20, 1);
+            AssertScoreIs(20);
+        }
+
+        [Test]
+        public void TestSpare()
+        {
+            _sut.Roll(4);
+            _sut.Roll(6);
+            _sut.Roll(1);
+            RollMany(17, 0);
+
+            AssertScoreIs(12);
+        }
+
+        [Test]
+        public void TestStrike()
+        {
+            _sut.Roll(10);
+            _sut.Roll(1);
+            _sut.Roll(2);
+            RollMany(16, 0);
+
+            AssertScoreIs(16);
+        }
+
+        [Test]
+        public void TestSpareLastFrame()
+        {
+            RollMany(18, 0);
+            _sut.Roll(4);
+            _sut.Roll(6);
+            _sut.Roll(1);
+
+            AssertScoreIs(11);
+        }
+
+        [Test]
+        public void TestStrikeLastFrame()
+        {
+            RollMany(16, 0);
+            _sut.Roll(10);
+            _sut.Roll(1);
+            _sut.Roll(2);
+
+            AssertScoreIs(14);
+        }
+
+        private void RollMany(int rolls, int pins)
+        {
+            for (var i = 0; i < rolls; i++)
+            {
+                _sut.Roll(pins);
+            }
+        }
+
+        private void AssertScoreIs(int expectedScore)
+        {
+            Assert.That(_sut.Score(), Is.EqualTo(expectedScore));
+        }
+    }
+}
---
+++ BowlingKata/Game.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+
+namespace BowlingKata
+{
+    public class Game
+    {
+        private const int TenPins = 10;
+        private readonly List<int> _pins = new List<int>();
+
+        public void Roll(int pins)
+        {
+            _pins.Add(pins);
+        }
+
+        public int Score()
+        {
+            var score = 0;
+
+            for (var roll = 0; roll < _pins.Count; roll++)
+            {
+                score += Score(roll);
+
+                if (IsSpareBonus(roll))
+                    return score;
+            }
+
+            return score;
+        }
+
+        private int Score(int roll)
+        {
+            if (IsSpare(roll))
+                return ScoreSpare(roll);
+
+            if (IsStrike(roll))
+            {
+                return ScoreStrike(roll);
+            }
+
+            return _pins[roll];
+        }
+
+        private bool IsSpare(int roll)
+        {
+            return roll > 0 && _pins[roll] + _pins[roll - 1] == TenPins;
+        }
+
+        private int ScoreSpare(int roll)
+        {
+            return _pins[roll] + _pins[roll + 1];
+        }
+
+        private bool IsStrike(int roll)
+        {
+            return _pins[roll] == TenPins;
+        }
+
+        private int ScoreStrike(int roll)
+        {
+            return _pins[roll] + _pins[roll + 1] + _pins[roll + 2];
+        }
+
+        private bool IsSpareBonus(int roll)
+        {
+            return IsSpare(roll) && roll == _pins.Count - 2;
+        }
+    }
+}

3cc86baf651341cd3bd88a90bd79f2f9c98d1b69
Graham Miller <G.Miller@craneware.com>
2020-08-20 11:53:07 +0100

    Complete

---
+++ BowlingKata.Test/GameTests.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework;
+
+namespace BowlingKata.Test
+{
+    [TestFixture]
+    public class GameTests
+    {
+        private Game _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _sut = new Game();
+        }
+
+        [Test]
+        public void TestGutterGame()
+        {
+            RollMany(20, 0);
+            Assert.That(_sut.Score(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void TestAllOnes()
+        {
+            RollMany(20, 1);
+            Assert.That(_sut.Score(), Is.EqualTo(20));
+        }
+
+        [Test]
+        public void TestOneSpare()
+        {
+            RollSpare();
+            _sut.Roll(3);
+            RollMany(17, 0);
+            Assert.That(_sut.Score(), Is.EqualTo(16));
+        }
+
+        [Test]
+        public void TestOneStrike()
+        {
+            RollStrike();
+            _sut.Roll(3);
+            _sut.Roll(4);
+            RollMany(16, 0);
+            Assert.That(_sut.Score(), Is.EqualTo(24));
+        }
+
+        [Test]
+        public void TestPerfectGame()
+        {
+            RollMany(12, 10);
+            Assert.That(_sut.Score(), Is.EqualTo(300));
+        }
+
+        private void RollMany(int rolls, int pins)
+        {
+            for (var i = 0; i < rolls; i++)
+            {
+                _sut.Roll(pins);
+            }
+        }
+
+        private void RollSpare()
+        {
+            _sut.Roll(5);
+            _sut.Roll(5);
+        }
+
+        private void RollStrike()
+        {
+            _sut.Roll(10);
+        }
+    }
+}
---
+++ BowlingKata/Game.cs
@@ -0,0 +1,65 @@
+﻿namespace BowlingKata
+{
+    public class Game
+    {
+        private readonly int[] _rolls = new int[21];
+        private int _currentRoll;
+
+        public void Roll(int pins)
+        {
+            _rolls[_currentRoll++] = pins;
+        }
+
+        public int Score()
+        {
+            var score = 0;
+            var rollIndex = 0;
+            
+            for (var frame = 0; frame < 10; frame++)
+            {
+                if (IsStrike(rollIndex))
+                {
+                    score += 10 + StrikeBonus(rollIndex);
+                    rollIndex++;
+                }
+                else if (IsSpare(rollIndex))
+                {
+                    score += 10 + SpareBonus(rollIndex);
+                    rollIndex += 2;
+                }
+                else
+                {
+                    score += PinsDroppedInFrame(rollIndex);
+                    rollIndex += 2;
+                }
+            }
+
+            return score;
+        }
+
+        private bool IsStrike(int rollIndex)
+        {
+            return _rolls[rollIndex] == 10;
+        }
+
+        private int StrikeBonus(in int rollIndex)
+        {
+            return _rolls[rollIndex + 1] + _rolls[rollIndex + 2];
+        }
+
+        private bool IsSpare(int rollIndex)
+        {
+            return _rolls[rollIndex] + _rolls[rollIndex+1] == 10;
+        }
+
+        private int SpareBonus(int rollIndex)
+        {
+            return _rolls[rollIndex + 2];
+        }
+
+        private int PinsDroppedInFrame(int rollIndex)
+        {
+            return _rolls[rollIndex] + _rolls[rollIndex+1];
+        }
+    }
+}
